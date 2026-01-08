596c7999e3b708a3fe0177f7c30fc63ffc76a6e5
Dr. Murtaza Ali Khan <drkhanmurtaza@gmail.com>
2026-01-08 17:37:02 +0000

    done

---
+++ CSCI201/PowTest1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double z = pow(3,2); 
+    std::cout << "z is " << z << std::endl;
+    return 0;
+}
--- CSCI201/PowTest2.cpp
+++ CSCI201/PowTest2.cpp
@@ -2,6 +2,6 @@
 #include <cmath>
 
 int main() {
-    std::cout << "Pow(5,3) is: " << pow(5,3) << std::endl;
+    std::cout << "Power Test: " << pow(3,2) << std::endl;
     return 0;
 }

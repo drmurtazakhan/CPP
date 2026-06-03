282c54eeb781c1e4f237c9c29e6dcf78a128d72b
drmurtazakhan <drkhanmurtaza@gmail.com>
2026-06-02 22:08:41 -0500

    d

---
+++ CSCI101/M3/HelpForLater.cpp
@@ -0,0 +1,25 @@
+// HelpForLater.cpp
+// compile: g++ HelpForLater.cpp -o HelpForLater.exe
+// run:     ./HelpForLater.exe
+// Demonstration of "held for later input"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout << "Enter AB: ";
+    cin >> ch;
+
+    cout << "The value stored in ch is: " << ch << endl;
+    cout << "The character 'B' is held for later input." << endl;
+
+    // Read the next character from the input buffer
+    cin >> ch;
+
+    cout << "The next input operation reads: " << ch << endl;
+
+    return 0;
+}

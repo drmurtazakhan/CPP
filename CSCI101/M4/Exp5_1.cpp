c403257d9076bd01c56a76b3a31337dc3e540f4f
drmurtazakhan <drkhanmurtaza@gmail.com>
2026-06-25 09:32:41 -0500

    d

---
+++ CSCI101/M4/Exp5_1.cpp
@@ -0,0 +1,30 @@
+/*
+ * File name: Exp5_1.cpp
+ * This program demonstrates a functional while loop.
+ * Compile: g++ Exp5_1.cpp -o Exp5_1.exe
+ * Run: ./Exp5_1.exe
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+
+    // The loop body consists of everything between { and }
+    while (i <= 20)
+    {
+        // These lines will execute as long as i is 20 or less
+        cout << i << " ";
+
+        // This is the update step (LCV). Without this, the loop
+        // would run forever because i would always be 0.
+        i = i + 5;
+
+        cout << endl;
+    }
+
+    return 0;
+}

9a3b609eb8178aae2b59158a5c341148c5937068
drmurtazakhan <drkhanmurtaza@gmail.com>
2026-08-21 17:52:54 -0500

    d

--- CSCI101/M1/PrePostIncrement.cpp
+++ CSCI101/M1/PrePostIncrement.cpp
@@ -1,6 +1,5 @@
-// Example2_20.cpp
-// compile: g++ Example2_20.cpp -o Example2_20.exe
-// run:     ./Example2_20.exe
+// compile: g++ PrePostIncrement.cpp -o PrePostIncrement.exe
+// run: ./PrePostIncrement.exe
 #include <iostream>
 using namespace std;
 
@@ -12,12 +11,11 @@ int main()
     // --- CASE 1: Pre-Increment (++a) ---
     // Rule: Increment FIRST, then use the new value in the calculation.
     a = 5;
-    b = 2 + (++a);
+    b = ++a;
 
     // Step-by-step breakdown:
     // 1. 'a' turns from 5 into 6 immediately.
-    // 2. The math becomes: b = 2 + 6;
-    // 3. 'b' becomes 8.
+    // 2. The math becomes: b = 6;
 
     cout << "--- Case 1 (Pre-Increment) ---" << endl;
     cout << "Value of a: " << a << endl;
@@ -27,11 +25,11 @@ int main()
     // --- CASE 2: Post-Increment (a++) ---
     // Rule: Use the CURRENT value in the calculation first, then increment 'a' AFTERWARDS.
     a = 5; // Resetting 'a' back to 5
-    b = 2 + (a++);
+    b = a++;
 
     // Step-by-step breakdown:
-    // 1. The math uses the current value of 'a' (which is 5): b = 2 + 5;
-    // 2. 'b' becomes 7.
+    // 1. The math uses the current value of 'a' (which is 5): b = 5;
+    // 2. 'b' becomes 5.
     // 3. Now that the line is done evaluating, 'a' secretly bumps up from 5 to 6.
 
     cout << "--- Case 2 (Post-Increment) ---" << endl;

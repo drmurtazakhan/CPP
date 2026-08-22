ed1f6b71384a24b0b6fedcdbcf38284597cfb56c
drmurtazakhan <drkhanmurtaza@gmail.com>
2026-08-22 10:58:32 -0500

    d

---
+++ CSCI201/ch06/tmp.cpp
@@ -0,0 +1,20 @@
+
+void calculate(int a, int b)
+{
+    // 2 formal parameters
+}
+
+void calculate(int a, int b, int c)
+{
+    // 3 formal parameters
+}
+
+void calculate(int a, double b)
+{
+    // int, double
+}
+
+void calculate(int a, int b)
+{
+    // int, int
+}

c8c3a9139d7e8e9ad70c225d0b526b5e5d473677
drmurtazakhan <drkhanmurtaza@gmail.com>
2026-09-19 22:56:33 -0500

    j

--- CSCI201/ch13/employee_comparison.cpp
+++ CSCI201/ch13/employee_comparison.cpp
@@ -1,9 +1,11 @@
-// File name: employee_comparison.cpp
+// compile: g++ employee_comparison.cpp -o employee_comparison.exe
+// run: ./employee_comparison.exe
 #include <iostream>
 #include <string>
 using namespace std;
 
-class Employee {
+class Employee
+{
 private:
     string name;
     double salary;
@@ -13,29 +15,34 @@ public:
 
     // 1. Using 'this' to access instance variables
     // No need to pass the object; 'this' points to the one that called the method.
-    void setSalary(double salary) {
+    void setSalary(double salary)
+    {
         // 'this->salary' is the instance variable
         // 'salary' is the local parameter
-        this->salary = salary; 
+        this->salary = salary;
     }
 
     // 2. Passing an object as a formal parameter
     // To compare, we are 'inside' one object and 'looking' at another.
-    bool hasHigherSalaryThan(Employee otherEmp) {
+    bool hasHigherSalaryThan(Employee otherEmp)
+    {
         // this->salary: Salary of the object that called the method
         // otherEmp.salary: Salary of the object passed in the parentheses
-        if (this->salary > otherEmp.salary) {
+        if (this->salary > otherEmp.salary)
+        {
             return true;
         }
         return false;
     }
 
-    void print() {
+    void print()
+    {
         cout << name << " ($" << salary << ")";
     }
 };
 
-int main() {
+int main()
+{
     Employee emp1("Alice", 50000);
     Employee emp2("Bob", 60000);
 
@@ -45,9 +52,12 @@ int main() {
 
     // Concept 2: Passing an object
     // emp1 is the "caller" (this), emp2 is the "parameter" (otherEmp)
-    if (emp1.hasHigherSalaryThan(emp2)) {
+    if (emp1.hasHigherSalaryThan(emp2))
+    {
         cout << "Alice makes more than Bob." << endl;
-    } else {
+    }
+    else
+    {
         cout << "Alice does not make more than Bob." << endl;
     }
 
